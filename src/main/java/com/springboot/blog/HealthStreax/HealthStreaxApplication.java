fac47b48b089e7d5ad4229ec2b321c5cb837ce3f
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-10-29 23:17:51 +0530

    initial commit

---
+++ src/main/java/com/springboot/blog/HealthStreax/HealthStreaxApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.blog.HealthStreax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthStreaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthStreaxApplication.class, args);
+	}
+
+}
